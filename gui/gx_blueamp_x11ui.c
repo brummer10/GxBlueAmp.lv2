d4abd04343729ac42f548a14dab66972be210cb7
brummer10 <brummer-@web.de>
2021-03-22 19:01:43 +0100

    Fix issue #33 ON/OFF toggle button doesn't work on several plugins

--- gui/gx_blueamp_x11ui.c
+++ gui/gx_blueamp_x11ui.c
@@ -704,7 +704,6 @@ static void check_value_changed(gx_blueampUI * const ui, int i, const float * co
 			ui->write_function(ui->controller,ui->controls[i].port,sizeof(float),0,value);
 		debug_print("send_controller_event for %i value %f\n",i,*(value));
 		send_controller_event(ui, i);
-		ui->block_event = -1;
 	}
 }
 
@@ -1075,6 +1074,7 @@ static void port_event(LV2UI_Handle handle, uint32_t port_index,
 		if (port_index == ui->controls[i].port) {
 			ui->block_event = (int)port_index;
 			check_value_changed(ui, i, &value);
+			ui->block_event = -1;
 		}
 	}
 }
