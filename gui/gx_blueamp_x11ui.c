52635d81b6784900b4c0e09bafa4bb737ed87ee0
Thorsten Maerz <info@netztorte.de>
2023-01-12 19:29:17 +0100

    remove old gtk workaround (reset_request())

--- gui/gx_blueamp_x11ui.c
+++ gui/gx_blueamp_x11ui.c
@@ -658,12 +658,6 @@ static void controller_expose(const gx_blueampUI * const ui,  const gx_controlle
 -----------------------------------------------------------------------
 ----------------------------------------------------------------------*/
 
-// hack to allow resizing below the initial size on gtk based hosts
-static void reset_request(gx_blueampUI * const ui) {
-	ui->resize->ui_resize(ui->resize->handle, ui->width/1.4, ui->height/1.4);
-	ui->first_resize = false;
-}
-
 // resize the xwindow and the cairo xlib surface
 static void resize_event(gx_blueampUI * const ui) {
 	XWindowAttributes attrs;
@@ -985,7 +979,6 @@ static void event_handler(gx_blueampUI * const ui) {
 					break;
 					case Button2:
 						// click on the mouse wheel
-						if (ui->first_resize) reset_request(ui);
 						//debug_print("Button2 \n");
 					break;
 					case Button3:
